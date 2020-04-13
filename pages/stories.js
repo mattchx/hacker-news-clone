3f0807ccafd7a446a610033ece1ad210511680da
Matthew Henderson <mattcharlesh@gmail.com>
2020-04-13 16:57:35 +0800

    enabled requesting data from API endpoint

--- pages/stories.js
+++ pages/stories.js
@@ -1,5 +1,30 @@
 import view from '../utils/view.js';
 
-export default function Stories(path) {
-  view.innerHTML = `<div>${path}</div>`;  
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+  
+  view.innerHTML = `<div>${hasStories ? stories.map(story =>
+    JSON.stringify(story)) : 'No stories'}</div>`;  
 }
+
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+  if (isHomeRoute) {
+    path = '/news';
+  } else if (isNewRoute) {
+    path = '/newest'
+  }
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
+}
+
+// https://node-hnapi.herokuapp.com/news
+
+// / (Top) -> /new
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
+// **jobs**
